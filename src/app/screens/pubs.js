6f95b78aa4da6c2f016c8c7e19b51fbad053766a
Eric George <eric@pokelondon.com>
2016-05-23 22:07:27 +0100

    slider updating date prop with moment

--- src/app/screens/pubs.js
+++ src/app/screens/pubs.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router'
+import moment from 'moment';
 
 import ThreeD from '../components/threed';
+import Slider from 'rc-slider';
 
 import { fetchBuildings } from '../services/overpass';
 import { getSuggestions, AFTERNOON } from '../services/api';
@@ -11,10 +13,13 @@ class Pub extends React.Component {
     constructor(props) {
         super(props);
         this.props = props;
+        this.moment = moment().startOf('day').add(8, 'hours');
         this.state = {
             items: [],
-            buildings: []
+            buildings: [],
+            date: this.moment.toDate()
         };
+        console.log(this.state.date);
     }
 
     componentDidMount() {
@@ -28,6 +33,12 @@ class Pub extends React.Component {
         let item = this.state.items[0];
         fetchBuildings(item.lat, item.lng)
             .then(buildings => this.setState({buildings}));
+    }
+
+    onSliderChange(value) {
+
+        let date = this.moment.clone().add(value, 'hours');
+        this.setState({date: date.toDate()});
     }
 
     render() {
@@ -45,10 +56,11 @@ class Pub extends React.Component {
                     <h2>{item.name}</h2>
                     <ThreeD
                         centre={{lat: item.lat, lng: item.lng}}
-                        date={new Date()}
+                        date={this.state.date}
                         buildings={this.state.buildings}
                     />
                     <p>Lorem ipsum</p>
+                    <Slider min={8} max={22} step={0.5} included={false} defaultValue={8} onChange={this.onSliderChange.bind(this)}/>
                 </div>
             )
         }
