a54c342d084f9ff96a7d58b83db5d237cbb601d2
Rich Savage <rich.savage@pokelondon>
2017-05-09 11:28:31 +0100

    dialog is pure component

--- src/app/components/dialog.js
+++ src/app/components/dialog.js
@@ -1,22 +1,22 @@
 import React from 'react';
 
-export default class Dialog extends React.Component {
-
-    constructor(props){
-        super(props);
-        this.props = props;
-    }
-
-    render(){
-        return (
-            <div className="Dialog">
-                <div className="Dialog-inner Box Box-row">
-                    <div className="Box Box-item">
-                        <p>{this.props.message}</p>
-                        <button className="Button--anchor" onClick={this.props.onButtonClick}>{this.props.buttonText}</button>
-                    </div>
+const Dialog = (props) => {
+    return (
+        <div className="Dialog">
+            <div className="Dialog-inner Box Box-row">
+                <div className="Box Box-item">
+                    <p>{props.message}</p>
+                    <button className="Button--anchor" onClick={props.onButtonClick}>{props.buttonText}</button>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
+
+Dialog.propTypes = {
+    message: React.PropTypes.string,
+    buttonText: React.PropTypes.string,
+    onButtonClick: React.PropTypes.func
+}
+
+export default Dialog;
