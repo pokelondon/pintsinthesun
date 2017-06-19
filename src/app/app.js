1d675e649f5e7f83e728b702744d6177dff0ff8c
Rich Savage <rich.savage@pokelondon>
2017-06-19 10:05:09 +0100

    persist recommendations to local storage

--- src/app/app.js
+++ src/app/app.js
@@ -6,6 +6,7 @@ import { Provider } from 'react-redux';
 import thunkMiddleware from 'redux-thunk';
 import { routerMiddleware } from 'react-router-redux'
 import createDebounce from 'redux-debounced'
+import { saveState, loadState} from './services/localStorage';
 
 import reducers from './reducers';
 
@@ -26,13 +27,25 @@ import promise from 'es6-promise';
 import 'isomorphic-fetch';
 promise.polyfill();
 
-
 const createStoreWithMiddleware = applyMiddleware(
     createDebounce(),
     thunkMiddleware,
     routerMiddleware(hashHistory),
 )(createStore);
-const store = createStoreWithMiddleware(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+const persistedState = loadState();
+
+const store = createStoreWithMiddleware(
+    reducers,
+    persistedState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+store.subscribe( () => {
+    saveState({
+        recommend: store.getState().recommend
+    });
+})
 
 if(window.location.hostname.indexOf('pintsinthesun.co.uk') != -1){
     GA.initialize('UA-15712565-30');
