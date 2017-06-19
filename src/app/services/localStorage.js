25db2f79a40ecdf3d89013cb893e5f335cacb1bc
Rich Savage <rich.savage@pokelondon>
2017-06-19 10:15:26 +0100

    comments!

--- src/app/services/localStorage.js
+++ src/app/services/localStorage.js
@@ -1,4 +1,7 @@
-
+/**
+* Load state object from localStorage
+* @return {object} - the state from localStorage
+*/
 export const loadState = () => {
     try {
         const serializedState = localStorage.getItem('state');
@@ -11,6 +14,10 @@ export const loadState = () => {
     }
 }
 
+/**
+* Save state from the store to localstorage
+* @param {object} state - the store state
+*/
 export const saveState = (state) => {
     try {
         const serializedState = JSON.stringify(state);
