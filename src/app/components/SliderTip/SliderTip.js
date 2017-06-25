f667216d9b3ff6583fc3e643fdf213f624d81719
Rich Savage <rich.savage@pokelondon>
2017-06-25 23:24:04 +0100

    fix slider tip position

--- src/app/components/SliderTip/SliderTip.js
+++ src/app/components/SliderTip/SliderTip.js
@@ -4,7 +4,10 @@ const SliderTip = (props) => {
     const currentHour = props.date.getHours();
     //The percentage of the current hour along the slider length, (7:00 to 21:00)
     //with 5px margin on left and right
-    const position = ((currentHour - 7) / (21 - 7)) * 90 + 5;
+    let position = ((currentHour - 7) / (21 - 7)) * 86 + 7;
+    position = position > 86 ? 86 : position;
+    position = position < 5 ? 5 : position;
+
     return(
         <div className="SliderTip">
             <span className="SliderTip-copy">Drag the slider to see how the shadows move through the day</span>
